a96ae038debf52c21139c937ce9ce7d258dd2b56
abdillahi Bashir <abdillahi.muse@hotmail.com>
2018-05-10 03:49:39 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
       self.receiver.balance += self.amount
       self.status = "complete"
     elsif
-      return "Transaction rejected. Please check your account balance."
+       "Transaction rejected. Please check your account balance."
     end
   end
 
